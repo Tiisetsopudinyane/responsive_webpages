f0df4bda1efad1f0e92ca243021c1cba663720f5
tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2023-05-25 15:55:24 +0200

    first responsive website commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from "./data.json"
+
+
+export default {
+  base : '/responsive_webpages/',
+  plugins: [handlebars({
+    context : data
+  })]
+};
