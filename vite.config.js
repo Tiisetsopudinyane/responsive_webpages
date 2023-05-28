d36fb6dbce909a0c26778367e6ff49f09c96ecd3
tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2023-05-28 10:27:14 +0200

    vite.config file modified

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+import data from "./data.json" assert {type:'json'};
 import handlebars from 'vite-plugin-handlebars';
-import data from "./data.json"
-
-
-export default {
+import { defineConfig } from 'vite'
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input : {
+        main: 'index.html',
+        one: 'index-bootstrap.html',
+        two: 'index-materialize.html',
+      }
+    },
+  },
   base : '/responsive_webpages/',
   plugins: [handlebars({
-    context : data
-  })]
-};
+    context : {
+      data  
+  }
+})],
+})
